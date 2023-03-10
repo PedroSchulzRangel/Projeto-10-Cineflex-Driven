318cd0b008fd07e762f7150afb6f0f44533121f6
Pedro Schulz e Silva Rangel <psrangel5002@hotmail.com>
2023-03-10 13:42:26 -0300

    Rotas e arquivo de cores de assento

---
+++ src/colors.js
@@ -0,0 +1,7 @@
+export const backgroundSelectedSeats = "#1AAE9E";
+export const borderSelectedSeats = "#0E7D71";
+export const backgroundAvailableSeats = "#C3CFD9";
+export const borderAvailableSeats = "#808F9D";
+export const backgroundUnavailableSeats = "#FBE192";
+export const borderUnavailableSeats = "#F7C52B";
+
